8fce7f05fb4ba62b77b63a76d44951612d70ea79
toastynerd <tylermorgan86@gmail.com>
2020-03-19 19:45:45 -0700

    added register definitions

---
+++ defs.asm
@@ -0,0 +1,17 @@
+;;;PPU Registers
+PPUCTL		= $2000
+PPUMASK		= $2001
+PPUSTATUS	= $2002
+OAMADDR		= $2003
+OAMDATA		= $2004
+PPUSCROLL	= $2005
+PPUADDR		= $2006
+PPUDATA		= $2007
+
+;;;TODO: APU registers
+
+;;;Other I/O Registers (still missing some)
+OAMDMA		= $4014
+APUSTATUS	= $4015
+JOYPAD1		= $4016
+JOYPAD2		= $4017
